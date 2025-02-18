125226c3c55c233f84901e1b0a9bcc81c2ec8333
Mustafa Mehmedoglu <mikbalm@gmail.com>
2025-02-17 22:07:21 -0500

    mock challange completed!

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,120 @@
 class NationalPark:
+    
+    all =[]
 
     def __init__(self, name):
         self.name = name
-        
+        NationalPark.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 3 <= len(value) and not hasattr(self, "name"):
+            self._name = value
+        else:
+            raise Exception("Park name must be a string longer than 3 characters.")
+
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.national_park is self]
+
     def visitors(self):
-        pass
-    
+        return list({trip.visitor for trip in self.trips()})
+
     def total_visits(self):
-        pass
-    
+        return len(self.trips())
+
     def best_visitor(self):
-        pass
+        visitors = [trip.visitor for trip in self.trips()]
+        return max(set(visitors), key=visitors.count)
+    
+    @classmethod
+    def most_visited(self):
+        return max(set(self.all), key=self.total_visits)
 
 
 class Trip:
     
+    all = []
+
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        Trip.all.append(self)
 
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, value):
+        if isinstance(value, str) and len(value) >= 7:
+            self._start_date = value
+        else:
+            raise Exception("Start date must be in the format 'Month Day' and be a valid date.")
+
+    @property
+    def end_date(self):
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, value):
+        if isinstance(value, str) and len(value) >= 7:
+            self._end_date = value
+        else:
+            raise Exception("End date must be in the format 'Month Day' and be a valid date.")
+    
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, value):
+        if isinstance(value, Visitor):
+            self._visitor = value
+        else:
+            raise Exception("Visitor must be an instance of the Visitor class.")
+
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, value):
+        if isinstance(value, NationalPark):
+            self._national_park = value
+        else:
+            raise Exception("National park must be an instance of the NationalPark class.")
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
-        
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise Exception("Visitor name must be a string between 1 and 15 characters.")
+
     def trips(self):
-        pass
-    
+        return [trip for trip in Trip.all if trip.visitor is self]
+
     def national_parks(self):
-        pass
-    
+        return list({trip.national_park for trip in self.trips()})
+
     def total_visits_at_park(self, park):
-        pass
+        total_visits = 0
+        for trip in self.trips():
+            if trip.national_park == park:
+                total_visits += 1
+        return total_visits
